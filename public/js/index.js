1c8f831943582bee25e80799fe1afe23a0998253
Dominic Prescod <dominicprescod@gmail.com>
2016-04-29 20:38:55 -0400

    resized

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,13 @@
 $(function(){
-  
+  var $logInButton = $('.login-signup a');
+  var $signUp = $('.login-signup button');
+
+  $logInButton.click(function(){
+    console.log($(this));
+  });
+
+  $signUp.click(function(){
+    console.log($(this));
+  });
+
 });
